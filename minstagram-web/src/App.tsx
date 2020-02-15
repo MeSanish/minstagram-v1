10cf6509d598aec5cef1d5098b372bec0b2d07bc
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-02-15 19:50:54 +0545

    added styled components : feat

--- minstagram-web/src/App.tsx
+++ minstagram-web/src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const AppSection = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
 
 const App: React.SFC<{}> = () => (
-  <h1>Minstagram v1</h1>
+  <AppSection>
+    <Title>Minstagram v1</Title>
+  </AppSection>
 );
 
 export default App;
