f7661520391eaaf3680c80fc58c30bf26fec7822
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-02-15 14:32:31 +0545

    send status to error when possible : refactor

--- minstagram-node/src/middleware/error.ts
+++ minstagram-node/src/middleware/error.ts
@@ -1,15 +1,22 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 
 interface IErrorRequest extends ErrorRequestHandler {
-  error: {
-    status: number;
-    message: string;
-  };
+  status: number;
+  message: string;
 }
 
 export function errorHandler(err: IErrorRequest, req: Request, res: Response, next: NextFunction) {
   console.log(err)
-  res.status(500).send({
-    message: `I did something stupid`
-  })
+  if (err.status && err.message) {
+    const { status, message } = err;
+    res.status(status).send({
+      message,
+    });
+  }
+  else {
+    res.status(500).send({
+      message: `I did something stupid`
+    })
+
+  }
 }
