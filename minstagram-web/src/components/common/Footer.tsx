df07123dbb3046a64c19f5255096bd156534fcac
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-02-17 00:53:10 +0545

    minor change : fix

--- minstagram-web/src/components/common/Footer.tsx
+++ minstagram-web/src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { history } from 'src/components/Router';
 
-const Footer = styled.div`
+const FooterWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 3fr);
   justify-items: center;
@@ -14,11 +14,11 @@ const Link = styled.a`
 const Footer = () => {
   const handleClick = (path: string) => () => history.push(path)
   return (
-    <Footer>
+    <FooterWrapper>
       <Link onClick={handleClick('/')}>home</Link>
       <Link onClick={handleClick('/post')}>post</Link>
       <Link onClick={handleClick('/profile')}>profile</Link>
-    </Footer>
+    </FooterWrapper>
   );
 };
 
