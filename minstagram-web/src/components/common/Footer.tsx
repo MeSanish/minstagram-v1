09ca67574e096dfc7b70de36893eafc37db003c9
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-02-17 00:51:21 +0545

    added links : feat

--- minstagram-web/src/components/common/Footer.tsx
+++ minstagram-web/src/components/common/Footer.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { history } from 'src/components/Router';
 
 const Footer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 3fr);
   justify-items: center;
 `
+const Link = styled.a`
+  cursor: pointer;
+`
 
-const Header = () => {
+const Footer = () => {
+  const handleClick = (path: string) => () => history.push(path)
   return (
     <Footer>
-      <div>home</div>
-      <div>post</div>
-      <div>profile</div>
+      <Link onClick={handleClick('/')}>home</Link>
+      <Link onClick={handleClick('/post')}>post</Link>
+      <Link onClick={handleClick('/profile')}>profile</Link>
     </Footer>
   );
 };
 
-export default Header;
+export default Footer;
