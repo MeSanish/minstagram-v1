8fe02185c07adb2bc551717a33cb0fa57de7a9da
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-02-18 21:58:05 +0545

    respond from api when online : refactor

--- minstagram-web/src/serviceworker/service.js
+++ minstagram-web/src/serviceworker/service.js
@@ -41,35 +41,37 @@ self.addEventListener('activate', event => {
   respondWith takes promise as argument
 */
 self.addEventListener('fetch', event => {
-  if (event.request.url.startsWith(self.location.origin)) { // checks for same origin i.e http://localhost:8000 in this case
-    event.respondWith((async () => {
-      try {
-        const sameOriginCachedReponse = await caches.match(event.request); // checks cache for current request
-        if (sameOriginCachedReponse) {
-          return sameOriginCachedReponse;
-        } else {
-          return await fetch(event.request);
+  if (event.request.method === 'GET') {
+    if (event.request.url.startsWith(self.location.origin)) { // checks for same origin i.e http://localhost:8000 in this case
+      event.respondWith((async () => {
+        try {
+          const sameOriginCachedReponse = await caches.match(event.request); // checks cache for current request
+          if (sameOriginCachedReponse && !navigator.onLine) {
+            return sameOriginCachedReponse;
+          } else {
+            return await fetch(event.request);
+          }
+        } catch (error) {
+          throw error;
         }
-      } catch (error) {
-        throw error;
-      }
-    })())
-  }
-  else {
-    event.respondWith((async () => {
-      try {
-        const crossOriginCachedResponse = await caches.match(event.request); // checks for cache for response
-        if(crossOriginCachedResponse) {
-          return crossOriginCachedResponse;
-        } else {
-          const corsCache = await caches.open(CORS);
-          const actualResponse = await fetch(event.request); // performs the actual fetch to backend
-          await corsCache.put(event.request, actualResponse.clone()); // fills the response to cache
-          return actualResponse;
+      })())
+    }
+    else {
+      event.respondWith((async () => {
+        try {
+          const crossOriginCachedResponse = await caches.match(event.request); // checks for cache for response
+          if (crossOriginCachedResponse && !navigator.onLine) {
+            return crossOriginCachedResponse;
+          } else {
+            const corsCache = await caches.open(CORS);
+            const actualResponse = await fetch(event.request); // performs the actual fetch to backend
+            await corsCache.put(event.request, actualResponse.clone()); // fills the response to cache
+            return actualResponse;
+          }
+        } catch (error) {
+          throw error;
         }
-      } catch (error) {
-        throw error;
-      }
-    })())
+      })())
+    }
   }
 });
