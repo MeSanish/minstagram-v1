615229b7f3c3a93871f6443d817c1335f03d6186
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-02-18 15:49:02 +0545

    updated to use async : refactor

--- minstagram-web/src/serviceworker/service.js
+++ minstagram-web/src/serviceworker/service.js
@@ -1,50 +1,75 @@
 const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const CORS = 'cors-cache-v1';
 
 const PRECACHE_URLS = [
   'index.html',
   'main.js'
 ]
 
+/*
+  respondWith takes promise as argument
+*/
+
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+  event.waitUntil((async () => {
+    try {
+      console.log('install....')
+      const cache = await caches.open(PRECACHE); // opens cache named PRECACHE
+      await cache.addAll(PRECACHE_URLS); // adds index and main.js to cache
+      return await self.skipWaiting();
+    } catch (error) {
+      throw error
+    }
+  })())
 });
 
 self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
+  const currentCaches = [PRECACHE, CORS]; // app cache array
+  event.waitUntil((async () => {
+    try {
+      console.log('activate....')
+      const cacheNames = await caches.keys()
+      const cachesToDelete = cacheNames.filter(cacheNames => !currentCaches.includes(cacheNames));
+      await Promise.all(cachesToDelete.map((cacheToDelete) => caches.delete(cacheToDelete)))
+    } catch (error) {
+      throw error;
+    }
+  })())
 });
 
+/*
+  respondWith takes promise as argument
+*/
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  // if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
+  if (event.request.url.startsWith(self.location.origin)) { // checks for same origin i.e http://localhost:8000 in this case
+    event.respondWith((async () => {
+      try {
+        const sameOriginCachedReponse = await caches.match(event.request); // checks cache for current request
+        if (sameOriginCachedReponse) {
+          return sameOriginCachedReponse;
+        } else {
+          return await fetch(event.request);
         }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  // }
+      } catch (error) {
+        throw error;
+      }
+    })())
+  }
+  else {
+    event.respondWith((async () => {
+      try {
+        const crossOriginCachedResponse = await caches.match(event.request); // checks for cache for response
+        if(crossOriginCachedResponse) {
+          return crossOriginCachedResponse;
+        } else {
+          const corsCache = await caches.open(CORS);
+          const actualResponse = await fetch(event.request); // performs the actual fetch to backend
+          await corsCache.put(event.request, actualResponse.clone()); // fills the response to cache
+          return actualResponse;
+        }
+      } catch (error) {
+        throw error;
+      }
+    })())
+  }
 });
