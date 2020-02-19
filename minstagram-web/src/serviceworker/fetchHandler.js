56c59064548a198ca99b6541f66a2409ce023beb
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2020-02-19 18:34:17 +0545

    redirect document destination to index.html : fix

--- minstagram-web/src/serviceworker/fetchHandler.js
+++ minstagram-web/src/serviceworker/fetchHandler.js
@@ -1,11 +1,20 @@
+const DESTINATION_DOCUMENT = "document";
+
 export const handleSameOriginRequests = async (event, PRECACHE) => {
   try {
-    const sameOriginCachedReponse = await caches.match(event.request); // checks cache for current request
-    if (sameOriginCachedReponse) {
-      fetchAndUpdateCache(event.request, PRECACHE)
-      return sameOriginCachedReponse;
+    if(event.request.destination === DESTINATION_DOCUMENT) {
+      const urlInstance = new URL(event.request.url); // redirects all urls to index for SPA
+      const origin = urlInstance.origin;
+      const cachedIndex = await caches.match(`${origin}/index.html`);
+      return cachedIndex;
     } else {
-      return await fetch(event.request);
+      const sameOriginCachedReponse = await caches.match(event.request); // checks cache for current request
+      if (sameOriginCachedReponse) {
+        fetchAndUpdate(event.request, PRECACHE)
+        return sameOriginCachedReponse;
+      } else {
+        return await fetch(event.request)
+      }
     }
   } catch (error) {
     throw error;
@@ -17,19 +26,19 @@ export const handleCrossOriginRequests = async (event, CORS) => {
   try {
     const crossOriginCachedResponse = await caches.match(event.request); // checks for cache for response
     if (crossOriginCachedResponse) {
-      fetchAndUpdateCache(event.request, CORS);
+      fetchAndUpdate(event.request, CORS);
       return crossOriginCachedResponse;
     } else {
-      return await fetchAndUpdateCache(event.request, CORS);
+      return await fetchAndUpdate(event.request, CORS);
     }
   } catch (error) {
     throw error;
   }
 }
 
-const fetchAndUpdateCache = async (request, cacheName) => {
+const fetchAndUpdate = async (request, cacheName) => {
   try {
-    if(navigator.onLine) {
+    if (navigator.onLine) {
       const actualResponse = await fetch(request); // performs the actual fetch to backend
       if (cacheName && actualResponse.status <= 200) {
         const cacheInstance = await caches.open(cacheName);
@@ -42,6 +51,6 @@ const fetchAndUpdateCache = async (request, cacheName) => {
       return;
     }
   } catch (error) {
-    throw error;
+    throw error
   }
 }
